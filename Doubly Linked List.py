1a16b7a974f24bd0b831086a3328feba014ff130
__Anonymous__ <38881836+HetShah004@users.noreply.github.com>
2019-06-19 18:13:00 +0530

    Update Doubly Linked List.py

--- Doubly Linked List.py
+++ Doubly Linked List.py
@@ -10,6 +10,7 @@ class Node:
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
+        self.length = 0
 
     def traverse(self):
         # Traverse through the list until we get None
@@ -27,8 +28,10 @@ class DoublyLinkedList:
         if self.head is None:
             new_node = Node(data)
             self.head = new_node
+            new_node.prev = self.head
+            self.length += 1
             return
-        print("List is not empty")
+        print("List is not empty.", "Try other functions")
 
     def append(self, data):
         # Inserts node at end
@@ -36,11 +39,13 @@ class DoublyLinkedList:
         crr = self.head
         if self.head is None:
             self.head = new_node
+            self.length += 1
             return
         while crr.next is not None:
             crr = crr.next
         crr.next = new_node
         new_node.prev = crr
+        self.length += 1
 
     def prepend(self, data):
         # Inserts node at beginning
@@ -49,39 +54,66 @@ class DoublyLinkedList:
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
+            self.length += 1
             return
         new_node.next = self.head
         self.head.prev = new_node
         self.head = new_node
+        self.length += 1
 
     def search(self, data):
         # A loop is used and break when we get the data of that node
         # If we get the data provided, we stop loop and return True or print
+        count = 0
         crr = self.head
         while crr.next is not None:
             if crr.data == data:
                 break
             crr = crr.next
+            count += 1
         if crr.data == data:
-            print(f"{data} is in the list")
+            print("At", count)
             return True
         else:
-            print(f"{data} is not in the list")
             return False
 
     def remove_head(self):
         # Just make next of head as head and now previous of head to none which will
         # delete the old head node
+        if self.head.next is None:
+            self.head = None
+            return
         self.head = self.head.next
         self.head.prev = None
+        self.length -= 1
 
     def remove_tail(self):
         # Loop until we get the last node
         # Make next of previous node to None
         crr = self.head
-        if self.head is None:
-            print("List is empty")
+        if self.head.next is None:
+            self.head = None
             return
         while crr.next is not None:
             crr = crr.next
         crr.prev.next = None
+        self.length -= 1
+
+    def insert_after_item(self, data, pos):
+        new_node = Node(data)
+        crr = self.head
+        if crr.data == pos:
+            self.head.next = new_node
+            new_node.prev = self.head
+            return
+        while crr is not None:
+            if crr.data == pos:
+                break
+            crr = crr.next
+        if crr is None:
+            print("List not in the item")
+            return
+        new_node.next = crr.next
+        new_node.prev = crr
+        crr.next = new_node
+        return
